3902712d9c1759d51aa3e6d1265acc8837ca8823
starkultra <prasathkathiresan11@gmail.com>
2023-06-25 10:48:28 +0530

    Added landing page

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello Welcome to Zindagi Na Milegi Dobara')
+})
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
